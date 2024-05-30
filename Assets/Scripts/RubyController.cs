7055af7b00183552ac8260d1df3151af4edc4bc3
Celmira Quaresma <CelmiraQuaresmaa@gmail.com>
2024-05-30 19:47:09 +0100

    Interações do Mundo - Zonas de Dano e Inimigos

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
-    int currentHealth;
+    public float timeInvincible = 2.0f;
+
     public int health { get { return currentHealth; } }
-    
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -16,9 +21,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
-       
     }
 
     // Update is called once per frame
@@ -26,6 +29,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -39,8 +49,19 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-
 }
+
+
+
